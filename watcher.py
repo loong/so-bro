567d24179d5026a8061fa56da149e5d5bdd43900
Long Hoang <long@mindworker.de>
2017-01-18 22:31:49 +0800

    Distinguish between script mode and interpreter mode

--- watcher.py
+++ watcher.py
@@ -6,12 +6,46 @@ from subprocess import Popen, PIPE
 
 import rpyc
 
+err = ""
+def handleInterpreter(conn, fd, data):
+    global err
+    
+    if fd == p.stderr.fileno():
+        datastr = str(data, 'utf8')
+
+        if datastr == '>>> ':
+            return
+        
+        if 'Type "help", "copyright", "credits" or "license" for more information.' in datastr:
+            return
+
+        err += datastr
+
+        # errors seem to always end with >>>
+        if '>>> ' in datastr:
+            conn.root.add_err(err)
+            err = ""
+
+def handleScript(conn, fd, data):
+    if fd == p.stderr.fileno():
+        # send to local debug service
+        conn.root.add_err(str(data, 'utf8'))
+
+def handle(conn, fd, data, mode):
+    if mode == 'interpreter':
+        handleInterpreter(conn, fd, data)
+    else:
+        handleScript(conn, fd, data)
+
+
 if __name__ == "__main__":
     conn = rpyc.connect("localhost", 18861)
     command = ['python']
+    mode = 'interpreter'
 
     if len(sys.argv) > 1:
         command = ['python'] + sys.argv[1:]
+        mode = 'script'
 
     with Popen(command, stdout=PIPE, stderr=PIPE) as p:
         readable = {
@@ -30,15 +64,4 @@ if __name__ == "__main__":
                 readable[fd].write(data)
                 readable[fd].flush()
 
-                # is stdout
-                if fd == p.stderr.fileno():
-                        
-                    datastr = str(data, 'utf8')
-                    if datastr == '>>> ':
-                        continue
-
-                    if 'Type "help", "copyright", "credits" or "license" for more information.' in datastr:
-                        continue
-                
-                    # send to local debug service
-                    conn.root.add_err(datastr)
+                handle(conn, fd, data, mode)
